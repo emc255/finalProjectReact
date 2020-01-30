b13377b989669cb3b97d3cece88620b9ad2cf885
emc <micha3l_255@yahoo.com>
2020-01-30 10:37:42 -0600

    Updates

---
+++ precache-manifest.608fbfc2d59c9afec3a7045244e9400f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea009c79e8e8fa82351619beecdd77a",
+    "url": "/finalProjectReact/index.html"
+  },
+  {
+    "revision": "a7a1d86cc9123fa36464",
+    "url": "/finalProjectReact/static/css/main.e0db2d8e.chunk.css"
+  },
+  {
+    "revision": "f3d41bcff3ac639430f3",
+    "url": "/finalProjectReact/static/js/2.d9c1eb74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/finalProjectReact/static/js/2.d9c1eb74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7a1d86cc9123fa36464",
+    "url": "/finalProjectReact/static/js/main.9c631bc2.chunk.js"
+  },
+  {
+    "revision": "4700f9a3372a3423f7b8",
+    "url": "/finalProjectReact/static/js/runtime-main.70892fd8.js"
+  }
+]);
